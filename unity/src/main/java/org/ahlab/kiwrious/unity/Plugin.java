834525a2d320d7ea95c61cf9549dc35bc1aa5e4e
bsc2fast <bsc2fast@hotmail.com>
2021-05-24 18:35:20 +0530

    initial commit

---
+++ unity/src/main/java/org/ahlab/kiwrious/unity/Plugin.java
@@ -0,0 +1,79 @@
+package org.ahlab.kiwrious.unity;
+
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Plugin {
+
+    private static final Plugin ourInstance = new Plugin();
+    private static  final  String LOGTAG = "KIWRIOUS";
+    private long startTime;
+
+    private float conductivity = 1.2f;
+    private float voc = 32;
+    private float uv = 2.0f;
+    private float lux = 80;
+    private float humidity = 70;
+    private float temperature = 30;
+    private float color_h = 102;
+    private float color_s = 102;
+    private float color_v = 103;
+
+    private boolean conductivity_online = true;
+    private boolean voc_online = true;
+    private boolean uv_lux_online = true;
+    private boolean humidity_temperature_online = true;
+    private boolean color_online = true;
+
+    public static Plugin getInstance() {
+        return ourInstance;
+    }
+
+    private Plugin(){
+        Log.i(LOGTAG, "Created Plugin");
+        startTime = System.currentTimeMillis();
+    }
+
+    public double getElapsedTime(){
+        return (System.currentTimeMillis()-startTime)/1000.0f;
+    }
+
+    public float getConductivity(){
+        return conductivity;
+    }
+
+    public float getVoc(){
+        return voc;
+    }
+
+    public float getUV(){
+        return uv;
+    }
+
+    public float getLux(){
+        return lux;
+    }
+
+    public float getHumidity(){
+        return humidity;
+    }
+
+    public float getTemperature(){
+        return temperature;
+    }
+
+    public float getColorH(){
+        return color_h;
+    }
+
+    public float getColorS(){
+        return color_s;
+    }
+
+    public float getColorV(){
+        return color_v;
+    }
+
+}
---
+++ unity/src/main/java/org/ahlab/kiwrious/unity/SensorData.java
@@ -0,0 +1,10 @@
+package org.ahlab.kiwrious.unity;
+
+import java.util.Dictionary;
+import java.util.Map;
+
+public class SensorData {
+    public boolean isOnline;
+    public int status;
+    public Map<String, Float> values;
+}
